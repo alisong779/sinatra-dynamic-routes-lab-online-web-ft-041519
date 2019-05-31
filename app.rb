4bf22fdb89db78b47e36deb22968f2cf34972d9c
Alison Golden <alisong779@yahoo.com>
2019-05-31 21:52:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     erb :say 
   end 
   
-  get 'say/:word1/:word2/:word3/:word4/:word5'
+  get 'say/:word1/:word2/:word3/:word4/:word5' do 
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
